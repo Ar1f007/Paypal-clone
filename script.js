417d4d16366943e8d2302066e02b2a7c51f19c8f
Ar1f007 <muhammadarif9416@yahoo.com>
2021-07-28 02:03:32 +0600

    menu onclick functionality

--- script.js
+++ script.js
@@ -16,6 +16,13 @@ dropdownItems.forEach(dropdownItem => {
     document.querySelector(".navbar-wrapper").style.background = "none";
   });
 });
+
+const menuIcon = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+});
 // End of Navigation
 
 // Pages
