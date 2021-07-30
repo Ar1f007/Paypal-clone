a995ab616a66df40419c6a124244802c73db86a2
Ar1f007 <muhammadarif9416@yahoo.com>
2021-07-31 01:01:03 +0600

    menu issue fixed

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// Pages
+document.querySelectorAll(".logo").forEach((logo) => {
+  logo.addEventListener("click", () => {
+    document.querySelector(".front-page").style.display = "block";
+    document.querySelector(".login-page").style.display = "none";
+    document.querySelector(".signup-page").style.display = "none";
+  });
+});
+
+document.querySelectorAll(".login").forEach((loginBtn) => {
+  loginBtn.addEventListener("click", () => {
+    document.querySelector(".front-page").style.display = "none";
+    document.querySelector(".login-page").style.display = "block";
+    document.querySelector(".signup-page").style.display = "none";
+  });
+});
+
+document.querySelectorAll(".signup").forEach((signupBtn) => {
+  signupBtn.addEventListener("click", () => {
+    document.querySelector(".front-page").style.display = "none";
+    document.querySelector(".login-page").style.display = "none";
+    document.querySelector(".signup-page").style.display = "flex";
+  });
+});
+// End of Pages
+
 // Navigation
 const dropdownItems = document.querySelectorAll(".dropdown-hover");
 
@@ -23,25 +49,22 @@ if (window.innerWidth < 1000) {
 
   document.querySelectorAll(".dropdown-heading-link").forEach((headingLink) => {
     headingLink.addEventListener("click", () => {
-      headingLink.parentElement.parentElement.style.left = "20rem";
+      headingLink.parentElement.parentElement.style.left = "-20rem";
     });
   });
 } else {
   dropdownItems.forEach((dropdownItem) => {
     dropdownItem.addEventListener("mouseover", () => {
       dropdownItem.lastElementChild.style.cssText =
-        "opacity: 1; visibility:visible";
-
+        "opacity: 1; visibility: visible";
       document.querySelector(".navbar-wrapper").style.background =
         "linear-gradient(to right, #066399, #2f8fdf, #066399)";
-
       dropdownItem.firstElementChild.firstElementChild.style.transform =
         "rotate(180deg)";
     });
     dropdownItem.addEventListener("mouseout", () => {
       dropdownItem.lastElementChild.style.cssText =
-        "opacity: 0; visibility:hidden";
-
+        "opacity: 0; visibility: hidden";
       document.querySelector(".navbar-wrapper").style.background = "none";
       dropdownItem.firstElementChild.firstElementChild.style.transform =
         "rotate(0)";
@@ -54,27 +77,3 @@ window.addEventListener("resize", () => {
 });
 
 // End of Navigation
-
-// Pages
-document.querySelectorAll(".logo").forEach((logo) => {
-  logo.addEventListener("click", () => {
-    document.querySelector(".front-page").style.display = "block";
-    document.querySelector(".login-page").style.display = "none";
-    document.querySelector(".signup-page").style.display = "none";
-  });
-});
-document.querySelectorAll(".login").forEach((loginBtn) => {
-  loginBtn.addEventListener("click", () => {
-    document.querySelector(".front-page").style.display = "none";
-    document.querySelector(".login-page").style.display = "block";
-    document.querySelector(".signup-page").style.display = "none";
-  });
-});
-document.querySelectorAll(".signup").forEach((signupBtn) => {
-  signupBtn.addEventListener("click", () => {
-    document.querySelector(".front-page").style.display = "none";
-    document.querySelector(".login-page").style.display = "none";
-    document.querySelector(".signup-page").style.display = "flex";
-  });
-});
-// End of Pages
